97cab1f8b44d0c4adc4a17cc1e0172c03a791c0a
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-04-29 15:00:28 -0400

    moves folders and files into the backend and frontend folders respectively

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :initiatives
   resources :jv_partners
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
