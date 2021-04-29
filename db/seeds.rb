4c02266639913bcad3c43273dff8cdd9880a9433
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-04-29 14:17:35 -0400

    creates commented out thought for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# initiatives = initiative.create([{name: "Prodigy Surge Joint Venture", objective: "FaceBook & Marketing Expansion"}])
+# jv_partners = jv_partner.create([{name: "Rodney Peak", level: "Co-Founder", responsibility: "Content", task: "Book"}])
+# jv_partners = jv_partner.create([{name: "James Hyatt", level: "Co-Founder", responsibility: "Automation", task: "Payment Gateway"}])
+# jv_partners = jv_partner.create([{name: "Deborah Waiters", level: "JV Partner", responsibility: "Customer Aquisition"}])
+# jv_partners = jv_partner.create([{name: "Kait Lemus", level: "JV Partner", responsibility: "Marketing", task: "FaceBook Ad Campaign"}])
+# jv_partners = jv_partner.create([{name: "Dustin Ball", level: "JV Partner", responsibility: "Lesson Creation", task: "Structure Courses"}])
+# jv_partners = jv_partner.create([{name: "Jason Moreta", level: "JV Partner", responsibility: "Spanish Aquisition", task: "Send out feelers into the community"}])
+# jv_partners = jv_partner.create([{name: "Anterio Jackson", level: "JV Partner", responsibility: "Course automation", task: "Set structure of course classroom"}])
+# jv_partners = jv_partner.create([{name: "Brent Bradford", level: "JV Partner", responsibility: "Customer Aquisition", task: "Send out feelers into the community"}])
+# jv_partners = jv_partner.create([{name: "Bill Julian", level: "CFO", responsibility: "Corporate Finances", task: "Open Bank Account"}])
+# jv_partners = jv_partner.create([{name: "Ebony DeBois", level: "JV Partner", responsibility: "Customer Aquisition", task: "Send out feelers into the community"}])
